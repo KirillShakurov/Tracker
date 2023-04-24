4347952d6a8e6cb9347850dfdf831cc364a777e9
Kirill Shakurov <kirill.shakurov@gmail.com>
2023-04-24 22:11:54 +0500

    Добавлены экраны онбординга, добавлена иконка приложения, исправлены цвета

---
+++ Tracker/Helpers/UserDefaultsBacked.swift
@@ -0,0 +1,23 @@
+//
+//  UserDefaultsBacked.swift
+//  Tracker
+//
+//  Created by Kirill on 24.04.2023.
+//
+
+import Foundation
+
+@propertyWrapper
+struct UserDefaultsBacked<Value> {
+    let key: String
+    let storage: UserDefaults = .standard
+    
+    var wrappedValue: Value? {
+        get {
+            storage.value(forKey: key) as? Value
+        }
+        set {
+            storage.setValue(newValue, forKey: key)
+        }
+    }
+}
